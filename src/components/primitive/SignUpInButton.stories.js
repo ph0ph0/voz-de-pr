c0c9c748712718ed5d607970945eef0f90ea981d
ph0ph0 <phillipalanlee@googlemail.com>
2019-10-08 22:12:47 +0100

    adding signupsignin button

---
+++ src/components/primitive/SignUpInButton.stories.js
@@ -0,0 +1,8 @@
+import SignUpInButton from './SignUpInButton.js'
+
+export default {
+    component: SignUpInButton,
+    title: 'SignUpInButton'
+}
+
+export const button = () => <SignUpInButton>Hey</SignUpInButton>
