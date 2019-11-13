69bf91ceafb9ad168bea869f53c252d986c2af76
ph0ph0 <phillipalanlee@googlemail.com>
2019-11-13 15:14:34 +0000

    removed the focus from the search bar in the navbar

---
+++ .history/src/components/Primitive/NavBar/SearchBar_20191028134954.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const SearchBar = styled.input`
+    height: 40px;
+    width: 90%;
+
+    padding-left: 20px;
+    margin-right: 10px;
+
+    border: 1px solid #f6f7f8;
+    border-radius: 2px;
+    background-color: #f6f7f8;
+
+    font-size: 14px;
+
+`
+
+export default SearchBar
