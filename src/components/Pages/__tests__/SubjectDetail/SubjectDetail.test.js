079c9b55aefa99fb9dde85d335020871824b827d
ph0ph0 <phillipalanlee@googlemail.com>
2019-12-05 10:19:30 +0000

    before branch change

--- src/components/Pages/__tests__/SubjectDetail/SubjectDetail.test.js
+++
@@ -1,9 +0,0 @@
-import { SubjectsMixed } from "../../../../Constants/MockSubjectsData";
-
-import { fetchSubject } from "../../SubjectDetail";
-
-global.log = () => {};
-
-it("fetches subject", () => {
-  expect(fetchSubject("1ABC")).toEqual(SubjectsMixed[0]);
-});
