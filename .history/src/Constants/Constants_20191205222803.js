287bc0dcd5d04469a56d1c7c25a0812120d5a36a
ph0ph0 <phillipalanlee@googlemail.com>
2019-12-05 22:28:20 +0000

    added in the new locations

---
+++ .history/src/Constants/Constants_20191205222803.js
@@ -0,0 +1,135 @@
+export const locations = [
+  {
+    id: 0,
+    title: "Aguadilla",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 1,
+    title: "Rincon",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 2,
+    title: "Mayagüez",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 3,
+    title: "Boqueron",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 4,
+    title: "Arecibo",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 5,
+    title: "Vega Baja",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 6,
+    title: "Dorado",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 7,
+    title: "Adjuntas",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 8,
+    title: "Jayuya",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 9,
+    title: "Aibonito",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 10,
+    title: "Cayey",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 11,
+    title: "Caguas",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 12,
+    title: "San Juan",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 13,
+    title: "Yuaco",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 14,
+    title: "Guanica",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 15,
+    title: "Ponce",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 16,
+    title: "Salinas",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 17,
+    title: "Guayama",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 18,
+    title: "Luquillo",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 19,
+    title: "Fajardo",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 20,
+    title: "Dewey",
+    selected: false,
+    key: "location"
+  },
+  {
+    id: 21,
+    title: "Isabel Segunda",
+    selected: false,
+    key: "location"
+  }
+];
+  
