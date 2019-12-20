9835f45b6ee02323c33625ce239d748499266104
ph0ph0 <phillipalanlee@googlemail.com>
2019-12-20 10:10:09 +0000

    before updating runtime

--- src/Utils/PictureManager.js
+++ src/Utils/PictureManager.js
@@ -8,17 +8,15 @@ export const savePictureWithSubjectId = async (image, subjectId) => {
   const lastDot = image.name.lastIndexOf(".");
   const extension = image.name.substring(lastDot);
   const key = uuidv4();
+  const folder = "subjectPictures";
 
-  const s3Output = await Storage.put(
-    `subjectPictures/${key}${extension}`,
-    image
-  );
+  const s3Output = await Storage.put(`${folder}/${key}${extension}`, image);
   const fileKey = s3Output.key;
 
   const picture = {
     id: key,
     subjectId: subjectId,
-    bucket: "vozdeprsubjectimagesstorages3service",
+    bucket: "voz-de-pr-mediastorages3-dev",
     region: "us-east-1",
     key: fileKey
   };
