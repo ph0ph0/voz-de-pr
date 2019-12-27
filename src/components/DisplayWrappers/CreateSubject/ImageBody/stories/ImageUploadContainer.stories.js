39260ad211aecd17b6d1aaa8bfd0b56ecd236214
ph0ph0 <phillipalanlee@googlemail.com>
2019-12-27 11:28:51 +0000

    passed all tests

--- src/components/DisplayWrappers/CreateSubject/ImageBody/stories/ImageUploadContainer.stories.js
+++ src/components/DisplayWrappers/CreateSubject/ImageBody/stories/ImageUploadContainer.stories.js
@@ -2,7 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import ImageUploadContainer from "../ImageUploadContainer";
-import mockApi from "../../API/__mocks__/mockApi";
+import { mockApi } from "components/DisplayWrappers/CreateSubject/API/__mocks__/mockApi";
 
 storiesOf(
   '"DisplayWrappers|CreateSubject/ImageBody/ImageUploadContainer',
