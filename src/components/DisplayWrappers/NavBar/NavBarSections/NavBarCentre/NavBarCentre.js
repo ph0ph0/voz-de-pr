1c0de132b3c216c2731aa66e3e747e49aa6555d3
ph0ph0 <phillipalanlee@googlemail.com>
2020-01-16 13:30:58 +0000

    building search

--- src/components/DisplayWrappers/NavBar/NavBarSections/NavBarCentre/NavBarCentre.js
+++ src/components/DisplayWrappers/NavBar/NavBarSections/NavBarCentre/NavBarCentre.js
@@ -3,27 +3,38 @@ import styled from "styled-components";
 
 import SearchBar from "../../../../Primitive/NavBar/SearchBar";
 
-const Wrapper = ({placeholder, ...props}) => {
+const Wrapper = ({ placeholder, ...props }) => {
+  const submit = event => {
+    window.log(`PRINT HIT`);
+    event.preventDefault();
+  };
+
+  const onChange = event => {
+    const searchText = event.target.value;
+    window.log(`searchText ${searchText}`);
+  };
+
   return (
     <div {...props}>
-      <SearchBar placeholder = {placeholder} type = "text"/>
+      <form onSubmit={event => submit(event)}>
+        <SearchBar placeholder={placeholder} type="text" onChange={onChange} />
+      </form>
     </div>
   );
 };
 
 const NavBarCentre = styled(Wrapper)`
-    /* background-color: red; */
-    height: 72px;
-    flex-grow: 10;
-    flex-shrink: 5;
-    flex-basis: 33.33333%;
-    /* flex-shrink: 2; */
-    /* width: 33.3333%; */
-    
+  /* background-color: red; */
+  height: 72px;
+  flex-grow: 10;
+  flex-shrink: 5;
+  flex-basis: 33.33333%;
+  /* flex-shrink: 2; */
+  /* width: 33.3333%; */
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default NavBarCentre;
