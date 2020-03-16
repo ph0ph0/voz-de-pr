ea30992b6bdc0972a98027a8f22df41486f78428
ph0ph0 <phillipalanlee@googlemail.com>
2020-03-16 10:41:37 +0000

    started adding react-helmet

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import theme from "./theme/Theme";
 
 import NavBar from "./components/DisplayWrappers/NavBar/NavBar";
 import Routes from "./Routes/Routes";
+import { Helmet } from "react-helmet";
 
 import { ApolloProvider } from "react-apollo";
 import { clientConfig } from "./clientConfig";
@@ -15,6 +16,18 @@ import { clientConfig } from "./clientConfig";
 import { UserProvider } from "./CustomHooks/user";
 import { SearchProvider } from "CustomHooks/useSearch";
 import { LanguageProvider } from "CustomHooks/useLanguage";
+
+const MetaTags = () => {
+  return (
+    <Helmet>
+      <title>Voz de Puerto Rico</title>
+      <meta
+        name="description"
+        content="Voz de Puerto Rico is a tool created by Puerto Ricans that allows users to take action for the development of a better Puerto Rico. We provide the space giving voice to all communities in need in order to form leaders committed to being an entity of change."
+      />
+    </Helmet>
+  );
+};
 
 function App() {
   return (
