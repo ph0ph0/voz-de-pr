2291b6c448836a33f600ea848677afc2b9364afe
ph0ph0 <phillipalanlee@googlemail.com>
2020-03-18 11:13:19 +0000

    trying to change title

--- src/components/Primitive/General/MetaTags.js
+++ src/components/Primitive/General/MetaTags.js
@@ -8,6 +8,7 @@ export const MetaTags = (title, description, url, image) => {
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
       <meta
         property="og:url"
         content={url ? url : "https://www.vozdepuertorico.com"}
